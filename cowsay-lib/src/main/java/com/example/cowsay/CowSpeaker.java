27fc25f19e4c0aef0f1be5b25fe30c6a053bd544
Chelsea Boling <chelsea@fossa.com>
2025-06-10 15:31:36 -0700

    Delete cowsay-lib directory

--- cowsay-lib/src/main/java/com/example/cowsay/CowSpeaker.java
+++
@@ -1,7 +0,0 @@
-package com.example.cowsay;
-
-public class CowSpeaker {
-    public static String say(String message) {
-        return String.format("  <%s>\n   \\   ^__^\n    \\  (oo)\\_______\n       (__)\\       )\\/\\\n           ||----w |\n           ||     ||", message);
-    }
-}
