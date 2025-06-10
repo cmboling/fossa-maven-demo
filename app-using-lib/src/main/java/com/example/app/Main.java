c3e05e2301490d0e64afadc8d64ab8e9d3ed1de4
Chelsea Boling <chelsea@fossa.com>
2025-06-10 15:31:26 -0700

    Delete app-using-lib directory

--- app-using-lib/src/main/java/com/example/app/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.app;
-
-import com.example.cowsay.CowSpeaker;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(CowSpeaker.say("Hello from cowsay!"));
-    }
-}
